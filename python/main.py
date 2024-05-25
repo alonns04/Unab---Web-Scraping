22488c7fe32c62753b0ae91e1c17f73d6c0a0bb4
alonns04 <claudiogabrielalonso@gmail.com>
2024-05-25 18:33:31 -0300

    Diccionario producto nombre + precio

--- python/main.py
+++ python/main.py
@@ -1,12 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-string_busqueda = input("¿Qué buscamos? ") # Dato a buscar
-r = requests.get('https://listado.mercadolibre.com.ar/{}'.format(string_busqueda.replace(' ', '-'), string_busqueda)) # Hace el request y reemplaza los espacios con "-"
-contenido = r.content # Ingresa al content del request "r"
+productos_array_meli = []
+precios_array_meli = []
 
-soup = BeautifulSoup(contenido, 'html.parser') # Parsea el contenido
+# string_busqueda = input("¿Qué buscamos? ") # Dato a buscar
+string_busqueda = "calculadora"
 
-products_array = []
+r_meli = requests.get('https://listado.mercadolibre.com.ar/{}'.format(string_busqueda.replace(' ', '-'), string_busqueda)) # Hace el request y reemplaza los espacios con "-"
+contenido_meli = r_meli.content # Ingresa al content del request "r"
 
-divs = []
+soup_meli = BeautifulSoup(contenido_meli, 'html.parser') # Parsea el contenido
+
+
+
+productos_meli = soup_meli.find_all('div', class_='ui-search-result__content-wrapper')
+
+
+for i in productos_meli:
+    producto = i.find("h2")
+    precio = i.find("span", class_="andes-money-amount__fraction")
+    if producto and precio:
+        productos_array_meli.append({"nombre": producto.text.strip(), "precio": precio.text.strip()})
+
+
+print(productos_array_meli)
