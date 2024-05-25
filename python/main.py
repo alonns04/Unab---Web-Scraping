e46dd7af4bb6dd2fc073554c188b8fb6df1e3eae
alonns04 <claudiogabrielalonso@gmail.com>
2024-05-25 19:02:34 -0300

    comentarios

--- python/main.py
+++ python/main.py
@@ -2,17 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 
-workbook = openpyxl.Workbook()
+workbook = openpyxl.Workbook() # Genera un excel
+hoja_activa = workbook.active # Abre el excel
 
-hoja_activa = workbook.active
+productos_array_meli = [] # Lista con los productos y sus características. precio, nombre, etc.
 
-productos_array_meli = []
-precios_array_meli = []
+string_busqueda = input("¿Qué buscamos?")
 
-# string_busqueda = input("¿Qué buscamos? ") # Dato a buscar
-string_busqueda = "calculadora"
-
-r_meli = requests.get('https://listado.mercadolibre.com.ar/{}'.format(string_busqueda.replace(' ', '-'), string_busqueda)) # Hace el request y reemplaza los espacios con "-"
+r_meli = requests.get('https://listado.mercadolibre.com.ar/{}'.format(string_busqueda.replace(' ', '-'), string_busqueda)) # Hace el request y reemplaza los espacios con "-" (No respeta las mayúsculas ni las minúsculas)
 contenido_meli = r_meli.content # Ingresa al content del request "r"
 
 soup_meli = BeautifulSoup(contenido_meli, 'html.parser') # Parsea el contenido
@@ -22,15 +19,15 @@ soup_meli = BeautifulSoup(contenido_meli, 'html.parser') # Parsea el contenido
 productos_meli = soup_meli.find_all('div', class_='ui-search-result__content-wrapper')
 
 
-for i in productos_meli:
+for i in productos_meli: # Itera en todo el código html
     producto = i.find("h2")
     precio = i.find("span", class_="andes-money-amount__fraction")
     if producto and precio:
         productos_array_meli.append({"nombre": producto.text.strip(), "precio": precio.text.strip()})
 
-# Guardar en el excel
 
-for i in range(len(productos_array_meli)):
+
+for i in range(len(productos_array_meli)): # Itera y lo va metiendo en el excel
     hoja_activa.cell(row = i + 1, column = 1, value = productos_array_meli[i]["nombre"])
     hoja_activa.cell(row = i + 1, column = 2, value = productos_array_meli[i]["precio"])
 
