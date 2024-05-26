d4b6ffd6dfaa02b1a6a799de9b7d36083cef57ec
alonns04 <claudiogabrielalonso@gmail.com>
2024-05-25 22:13:52 -0300

    links agregados

--- python/main.py
+++ python/main.py
@@ -32,18 +32,16 @@ for i in productos_meli: # Itera en todo el código html
     producto = i.find("h2")
     moneda = i.find("span", class_="andes-money-amount__currency-symbol")
     precio = i.find("span", class_="andes-money-amount__fraction")
+    link = i.find("a")["href"]
     if producto and precio:
-        productos_array_meli.append({"nombre": producto.text.strip(), "precio": precio.text.strip(), "moneda": moneda.text.strip()})
-
+        productos_array_meli.append({"nombre": producto.text.strip(), "precio": precio.text.strip(), "moneda": moneda.text.strip(), "link": link})
 
 
 for i in range(len(productos_array_meli)): # Itera y lo va metiendo en el excel
     hoja_activa.cell(row = i + 1, column = 1, value = productos_array_meli[i]["nombre"])
     hoja_activa.cell(row = i + 1, column = 2, value =  productos_array_meli[i]["moneda"])
     hoja_activa.cell(row = i + 1, column = 3, value =  productos_array_meli[i]["precio"])
-
+    hoja_activa.cell(row = i + 1, column = 4, value =  productos_array_meli[i]["link"])
 
 ruta_guardado = '../excel/producto_{}.xlsx'.format(string_busqueda)
 workbook.save(ruta_guardado)
-
-print('https://listado.mercadolibre.com.ar/{}#D[A:{}]'.format(meli_string1(string_busqueda), meli_string2(string_busqueda)))
