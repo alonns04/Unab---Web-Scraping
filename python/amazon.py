5955d3a40c258d92c9c6c577d8df23dc4dfe903a
alonns04 <claudiogabrielalonso@gmail.com>
2024-06-12 00:10:25 -0300

    Amazon

---
+++ python/amazon.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+import smtplib
+
+
+class Amazon():
+    def __init__(self, product_name: str):
+        self.product_name = product_name
+        self.array_products = [] # Lista con los productos y sus características. precio, nombre, etc.
+        self.link_product = self.generar_link_amazon()
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0", "Accept-Encoding":"gzip, deflate, br, zstd", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+        self.requests = requests.get(self.get_link_product(), headers=self.headers)
+        self.html_content = BeautifulSoup(self.requests.content, "html.parser")
+        self.divs = self.html_content.find_all("div", class_="sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small gsx-ies-anchor sg-col-12-of-16")
+
+    def get_link_product(self):
+        return self.link_product
+        
+    def generar_link_amazon(self):
+        texto_limpio = self.product_name.strip().replace(" ", "+")
+        base_url = "https://www.amazon.com/s?k="
+        url_completa = f"{base_url}{texto_limpio}&__mk_es_US=ÅMÅŽÕÑ&crid=&sprefix=&ref=nb_sb_noss"
+        return url_completa
+    
+    
+    def create_filter_list(self):from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+import smtplib
+
+
+class Amazon():
+    def __init__(self, product_name: str):
+        self.product_name = product_name
+        self.array_products = [] # Lista con los productos y sus características. precio, nombre, etc.
+        self.link_product = self.generar_link_amazon()
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0", "Accept-Encoding":"gzip, deflate, br, zstd", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+        self.requests = requests.get(self.get_link_product(), headers=self.headers)
+        self.html_content = BeautifulSoup(self.requests.content, "html.parser")
+        self.divs = self.html_content.find_all("div", class_="sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small gsx-ies-anchor sg-col-12-of-16")
+
+    def get_link_product(self):
+        return self.link_product
+        
+    def generar_link_amazon(self):
+        texto_limpio = self.product_name.strip().replace(" ", "+")
+        base_url = "https://www.amazon.com/s?k="
+        url_completa = f"{base_url}{texto_limpio}&__mk_es_US=ÅMÅŽÕÑ&crid=&sprefix=&ref=nb_sb_noss"
+        return url_completa
+    
+    
+    def create_filter_list(self):
+        for i in self.divs: # Itera en los divs de la página y extra la información
+            producto = i.find("span", class_="a-size-medium a-color-base a-text-normal")
+            link = "https://www.amazon.com/-/es" + i.find("a", class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal").get("href")
+            price = i.find("span", class_="a-price-whole")
+            cents = i.find("span", class_="a-price-fraction")
+            print(cents)
+            if price and price.text:
+                price = price.text.split(".")[0] 
+                if cents:
+                    price = int(price) + int(cents.text) / 100      
+            self.array_products.append({"nombre": producto.text, "precio": price})
+    
+producto = Amazon("calculadora")
+
+print(producto.divs)
+print(len(producto.divs))
+    
+producto = Amazon("calculadora")
+
+print(producto.divs)
+print(len(producto.divs))
+producto.create_filter_list()
+print(producto.array_products)
